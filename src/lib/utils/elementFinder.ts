643f02224e7152e36a63d56765fe31e1a7274482
eclipher <131247135+eclipher@users.noreply.github.com>
2025-04-07 19:11:30 +0800

    feat: create element finder

---
+++ src/lib/utils/elementFinder.ts
@@ -0,0 +1,82 @@
+type Config = {
+    /** Maximum time to wait in milliseconds */
+    maxWaitTime: number;
+    onError?: (error: Error) => void;
+    onAllFound?: (els: Element[]) => void;
+};
+
+const DEFAULT_CONFIG: Config = {
+    maxWaitTime: 10000,
+};
+
+export class ElementFinder {
+    targets: { selector: string; element: Element | null }[] = [];
+
+    constructor(
+        /** The CSS selector strings to look for */
+        selectors: string[] = [],
+        public config: Config = DEFAULT_CONFIG
+    ) {
+        this.targets = selectors.map((selector) => ({
+            selector,
+            element: null,
+        }));
+    }
+
+    init() {
+        // First try direct check
+        try {
+            this.#findAllElements();
+
+            if (!this.#allIsFound) {
+                this.setupMutationObserver();
+            }
+        } catch (err) {
+            console.error("Error in ElementFinder:", err);
+            if (err instanceof Error) this.config.onError?.(err);
+            else throw err;
+        }
+    }
+
+    /** Try to find all elements by selector, and store them in `targets` if found.
+     * If all elements are found, invoke the `onAllFound` callback. */
+    #findAllElements() {
+        this.targets.forEach(({ selector, element }, index) => {
+            if (element) return;
+            this.targets[index].element = document.querySelector(selector);
+        });
+
+        if (this.#allIsFound) {
+            const elements = this.targets.map((t) => t.element!);
+            this.config.onAllFound?.(elements);
+        }
+    }
+
+    /** Check if all target elements have been found */
+    get #allIsFound() {
+        return this.targets.every(({ element }) => element !== null);
+    }
+
+    setupMutationObserver() {
+        const callback: MutationCallback = (_mutations, obs) => {
+            this.#findAllElements();
+            // Stop observing once all elements are found
+            if (this.#allIsFound) obs.disconnect();
+        };
+
+        const observer = new MutationObserver(callback);
+
+        observer.observe(document.body, {
+            childList: true, // Watch for changes in direct children
+            subtree: true, // Watch for changes in the entire subtree
+        });
+
+        // Set a timeout to stop the observer after maxWaitTime
+        setTimeout(() => {
+            observer.disconnect();
+            throw new Error(
+                `Timeout: Not all elements found within ${this.config.maxWaitTime}ms`
+            );
+        }, this.config.maxWaitTime);
+    }
+}
