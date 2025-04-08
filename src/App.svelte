742240ebb2745d2eb6aa3ad82a9d59bbf5adb757
eclipher <131247135+eclipher@users.noreply.github.com>
2025-04-08 16:39:26 +0800

    feat: format on save

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,20 @@
     import CopyTitle from "./copy-title.svelte";
     import CopyDescription from "./copy-description.svelte";
     import DownloadAsJupyter from "./download-as-jupyter.svelte";
+    import { findElement } from "./lib/utils/elementFinder";
+
+    // format on save
+    document.addEventListener("keydown", async (e) => {
+        const formatBtn = (await findElement(
+            "div.flex.items-center.gap-1 > button:nth-child(1)",
+            { timeout: 500 }
+        )) as HTMLButtonElement;
+        if (e.ctrlKey && e.key === "s") {
+            e.preventDefault();
+            formatBtn.click();
+            console.log("Format button clicked");
+        }
+    });
 </script>
 
 <div>
