597f2f10c22f0ec2692fe980776d823d4af1b9b7
eclipher <131247135+eclipher@users.noreply.github.com>
2025-04-21 18:39:58 +0800

    fix: add table support and img URL conversion to htmlToMd

--- src/lib/utils/htmlToMd.ts
+++ src/lib/utils/htmlToMd.ts
@@ -1,23 +1,37 @@
 import TurndownService from "turndown";
+import { tables } from "@joplin/turndown-plugin-gfm";
 
 export function createTurndownService() {
-    return new TurndownService({
-        emDelimiter: "*",
-        bulletListMarker: "-",
-    })
-        .addRule("pre", {
-            filter: ["pre"],
-            replacement: (_content, node) =>
-                "\n```txt\n" + node.textContent?.trim() + "\n```\n",
+    return (
+        new TurndownService({
+            emDelimiter: "*",
+            bulletListMarker: "-",
         })
-        .addRule("superscript", {
-            filter: ["sup"],
-            replacement: (content) => "^" + content,
-        })
-        .addRule("paragraph", {
-            filter: ["p"],
-            replacement: (content) => "\n\n" + content + "\n\n",
-        });
+            .addRule("pre", {
+                filter: ["pre"],
+                replacement: (_content, node) =>
+                    "\n```txt\n" + node.textContent?.trim() + "\n```\n",
+            })
+            .addRule("superscript", {
+                filter: ["sup"],
+                replacement: (content) => "^" + content,
+            })
+            .addRule("paragraph", {
+                filter: ["p"],
+                replacement: (content) => "\n\n" + content + "\n\n",
+            })
+            // convert img link from relative path to absolute path
+            .addRule("convert-path-for-img", {
+                filter: ["img"],
+                replacement: (_content, node) => {
+                    // important: directly access DOM property (fully resolved absolute URL)
+                    // instead of `getAttribute()` (literal attribute value)
+                    const { src, alt } = node as HTMLImageElement;
+                    return `![${alt}](${src})`;
+                },
+            })
+            .use(tables)
+    ); // parse table
 }
 
 export function htmlToMd(
