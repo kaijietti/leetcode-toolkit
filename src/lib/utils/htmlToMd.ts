8850e090d829821811e86a88640cf5ba210c7f0f
eclipher <131247135+eclipher@users.noreply.github.com>
2025-04-21 17:48:48 +0800

    refactor: make htmlToMd configurable

--- src/lib/utils/htmlToMd.ts
+++ src/lib/utils/htmlToMd.ts
@@ -1,23 +1,28 @@
 import TurndownService from "turndown";
 
-const turndownService = new TurndownService({
-    emDelimiter: "*",
-    bulletListMarker: "-",
-});
-turndownService.addRule("pre", {
-    filter: ["pre"],
-    replacement: (content, node) =>
-        "\n```txt\n" + node.textContent?.trim() + "\n```\n",
-});
-turndownService.addRule("superscript", {
-    filter: ["sup"],
-    replacement: (content) => "^" + content,
-});
-turndownService.addRule("paragraph", {
-    filter: ["p"],
-    replacement: (content) => "\n\n" + content + "\n\n",
-});
+export function createTurndownService() {
+    return new TurndownService({
+        emDelimiter: "*",
+        bulletListMarker: "-",
+    })
+        .addRule("pre", {
+            filter: ["pre"],
+            replacement: (_content, node) =>
+                "\n```txt\n" + node.textContent?.trim() + "\n```\n",
+        })
+        .addRule("superscript", {
+            filter: ["sup"],
+            replacement: (content) => "^" + content,
+        })
+        .addRule("paragraph", {
+            filter: ["p"],
+            replacement: (content) => "\n\n" + content + "\n\n",
+        });
+}
 
-export function htmlToMd(htmlStr: string | TurndownService.Node) {
-    return turndownService.turndown(htmlStr);
+export function htmlToMd(
+    htmlStr: string | TurndownService.Node,
+    service = createTurndownService()
+) {
+    return service.turndown(htmlStr);
 }
